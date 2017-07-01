4cabd53cae4fd82a77321eba92cc5f8df49f0592
Ajay NS <ajay.ns@outlook.com>
2017-07-01 16:39:53 +0530

    Add functions

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function showPreview() {
+	document.getElementById('previewer-img').src = "http://via.placeholder.com/350x150";
+	document.getElementById('previewer').style.display = "block";
+}
+
+function closePreview() {
+	document.getElementById('previewer').style.display = "none";
+}
