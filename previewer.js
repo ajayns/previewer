2f185a715e2ed18f25088419b9bccc5d2fa1e436
Ajay NS <ajay.ns@outlook.com>
2017-07-02 19:57:20 +0530

    Clean code, add comments

--- previewer.js
+++ previewer.js
@@ -1,5 +1,15 @@
+/*!
+ * previewer.js v0.0.9
+ * https://github.com/ajayns/previewer
+ *
+ * Copyright (c) 2017 Ajay NS
+ * Released under the MIT license
+ */
+
+// PREVIEWER CLASS
 function Previewer() {
 
+	// Previewer html template
 	this.template = '<div id="previewer">' +
 		'		<div id="previewer-fade"></div>' +
 		'		<img src="" alt="" id="previewer-img">' +
@@ -7,19 +17,18 @@ function Previewer() {
 		'			<span>×</span>' +
 		'		</div>' +
 		'	</div>';
+	
+	// Add it to the body on init
 	$("body").append(this.template);
 
-	$(".preview-img").click(function () {
+	// Get img src and call previewer on click
+	$(".preview-img, .preview-imgs>li>img").click(function () {
 		var imgSrc = $(this).attr("src");
 		$("#previewer-img").attr("src", imgSrc);
 		$("#previewer").css("display", "block");
 	});
 	
-	$(".preview-imgs>li>img").click(function () {
-		$("#previewer-img").attr("src", $(this).attr("src"));
-		$("#previewer").css("display", "block");
-	});
-
+	// Close previewer on click
 	$("#previewer-close").click(function () {
 		$("#previewer").css("display", "none");
 	});
