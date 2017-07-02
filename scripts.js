fb20024699ab60367700388ec4b9d60f42602f00
Ajay NS <ajay.ns@outlook.com>
2017-07-02 13:30:59 +0530

    Wrap library as object

--- scripts.js
+++ scripts.js
@@ -1,3 +1,31 @@
+function Previewer() {
+
+	this.template = '<div id="previewer">' +
+		'		<div id="previewer-fade"></div>' +
+		'		<img src="" alt="" id="previewer-img">' +
+		'		<div id="previewer-close">' +
+		'			<span>×</span>' +
+		'		</div>' +
+		'	</div>';
+	$("body").append(this.template);
+
+	$(".preview-img").click(function () {
+		$("#previewer-img").attr("src", $(this).attr("src"));
+		$("#previewer").css("display", "block");
+	});
+
+	$("#previewer-close").click(function () {
+		$("#previewer").css("display", "none");
+	});
+
+}
+
+Previewer.prototype.showPreview = function (imgSrc) {
+	$("#previewer-img").attr("src", imgSrc);
+	$("#previewer").css("display", "block");
+}
+
+/*
 function constructor() {
 	var template = '<div id="previewer">' +
 		'		<div id="previewer-fade"></div>' +
@@ -10,14 +38,14 @@ function constructor() {
 	$(".preview-img").click(function () {
 		showPreview($(this).attr("src"));
 	})
-	
+
 	$("#previewer-close").click(function () {
-		$("#previewer").css("display","none");
-	});	
+		$("#previewer").css("display", "none");
+	});
 }
 
 function showPreview(imgSrc) {
 	$("#previewer-img").attr("src", imgSrc);
 	$("#previewer").css("display", "block");
 }
-
+*/
