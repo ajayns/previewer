1fa36f0de574aac2327e0dd159cfc77c07e14c54
haideralipunjabi <haiderali@cyberservices.com>
2017-07-06 08:22:27 +0530

    Grouped close functions

--- previewer.js
+++ previewer.js
@@ -9,47 +9,41 @@
 // PREVIEWER CLASS
 function Previewer() {
 
-	// Previewer html template
-	this.template = '<div id="previewer">' +
-		'		<div id="previewer-fade"></div>' +
-		'		<img src="" alt="" id="previewer-img">' +
-		'		<div id="previewer-close">' +
-		'			<span>×</span>' +
-		'		</div>' +
-		'	</div>';
+  // Previewer html template
+  this.template = '<div id="previewer">' +
+    '		<div id="previewer-fade"></div>' +
+    '		<img src="" alt="" id="previewer-img">' +
+    '		<div id="previewer-close">' +
+    '			<span>×</span>' +
+    '		</div>' +
+    '	</div>';
 
-	this.closeWindow = function () {
-		$("#previewer").css("display", "none");
-		$("body").css("overflow", "scroll");
-	}
+  this.closeWindow = function() {
+    $("#previewer").css("display", "none");
+    $("body").css("overflow", "scroll");
+  };
 
-	var self = this;
-	// Add it to the body on init
-	$("body").append(this.template);
+  var self = this;
+  // Add it to the body on init
+  $("body").append(this.template);
 
-	// Get img src and call previewer on click
-	$(".preview-image, .preview-images>li>img").click(function () {
-		var imgSrc = $(this).attr("src");
-		$("body").css("overflow", "hidden");
-		$("#previewer-img").attr("src", imgSrc);
-		$("#previewer").css("display", "block");
-	});
+  // Get img src and call previewer on click
+  $(".preview-image, .preview-images>li>img").click(function() {
+    var imgSrc = $(this).attr("src");
+    $("body").css("overflow", "hidden");
+    $("#previewer-img").attr("src", imgSrc);
+    $("#previewer").css("display", "block");
+  });
 
-	// Close previewer on click
-	$("#previewer-close").click(function () {
-		self.closeWindow();
-	});
-	
-	// Close previewer on click outside #previewer-img
-	$("#previewer-fade").click(function () {
-		self.closeWindow();
-	});
-	
+  // Close previewer when clicked
+  $("#previewer-close, #previewer-fade").click(function() {
+    self.closeWindow();
+  });
 
-	// Close window on keypress
-	$(document).keyup(function (e) {
-		if (e.keyCode == 27) { // escape key maps to keycode `27`
-			self.closeWindow();
-		}
-	});
+  // Close window on keypress
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) { // escape key maps to keycode `27`
+      self.closeWindow();
+    }
+  });
 }
