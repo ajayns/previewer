b423245627deed5691798cec10168e4649245232
Ajay NS <ajay.ns@outlook.com>
2017-07-06 19:22:50 +0530

    Remove jQuery dependancy

--- previewer.js
+++ previewer.js
@@ -9,39 +9,68 @@
 // PREVIEWER CLASS
 function Previewer() {
 
-  // Previewer html template
-  this.template = '<div id="previewer">' +
-    '		<div id="previewer-fade"></div>' +
-    '		<img src="" alt="" id="previewer-img">' +
-    '		<div id="previewer-close">' +
-    '			<span>×</span>' +
-    '		</div>' +
-    '	</div>';
+	// Previewer html template
+	this.template = '<div id="previewer">' +
+		'		<div id="previewer-fade"></div>' +
+		'		<img src="" alt="" id="previewer-img">' +
+		'		<div id="previewer-close">' +
+		'			<span>×</span>' +
+		'		</div>' +
+		'	</div>';
 
-  this.closeWindow = function() {
-    $("#previewer").css("display", "none");
-    $("body").css("overflow", "scroll");
-  };
 
-  var self = this;
-  // Add it to the body on init
-  $("body").append(this.template);
+	// Add it to the body on init
+	// $("body").append(this.template);
+	document.body.innerHTML += this.template;
 
-  // Get img src and call previewer on click
-  $(".preview-image, .preview-images img").click(function() {
-    var imgSrc = $(this).attr("src");
-    $("body").css("overflow", "hidden");
-    $("#previewer-img").attr("src", imgSrc);
-    $("#previewer").css("display", "block");
-  });
+	// Define function to close preview
+	this.closeWindow = function () {
+		// $("#previewer").css("display", "none");
+		// $("body").css("overflow", "scroll");
 
-  // Close previewer when clicked
-  $("#previewer-close, #previewer-fade").click(this.closeWindow);
+		document.getElementById("previewer").style.display = "none";
+		document.body.style.overflow = "scroll";
+	};
 
-  // Close window on keypress
-  $(document).keyup(function(e) {
-    if (e.keyCode == 27) { // escape key maps to keycode `27`
-      self.closeWindow();
-    }
-  });
+	var self = this;
+
+	// Get img src and call previewer on click
+	/*
+	$(".preview-image, .preview-images img").click(function () {
+		var imgSrc = $(this).attr("src");
+		$("body").css("overflow", "hidden");
+		$("#previewer-img").attr("src", imgSrc);
+		$("#previewer").css("display", "block");
+	});
+	*/
+	var images = document.querySelectorAll(".preview-image, .preview-images img");
+	for (i = 0; i < images.length; i++) {
+		images[i].addEventListener('click', function () {
+			var imgSrc = this.getAttribute("src");
+			document.getElementById("previewer-img").src = imgSrc;
+			document.getElementById("previewer").style.display = "block";
+		});
+	}
+
+
+	// Close previewer when clicked
+	// $("#previewer-close, #previewer-fade").click(this.closeWindow);
+	document.getElementById("previewer-close").addEventListener("click", this.closeWindow);
+	document.getElementById("previewer-fade").addEventListener("click", this.closeWindow);
+
+
+	// Close window on keypress
+	/*
+	$(document).keyup(function (e) {
+		if (e.keyCode == 27) { // escape key maps to keycode `27`
+
+		}
+	});
+	*/
+	document.onkeydown = function (evt) {
+		evt = evt || window.event;
+		if (evt.keyCode == 27) {
+			self.closeWindow();
+		}
+	};
 }
